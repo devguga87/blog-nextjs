194a094ca6b22427bbbd0afacf56064459f5514a
Gustavo Seabra <devguga87@gmail.com>
2021-11-10 20:44:00 -0300

    feature: component ArticleMeta

---
+++ src/components/ArticleMeta/mock.ts
@@ -0,0 +1,22 @@
+import { ArticleMetaProps } from '.';
+
+export default {
+  createdAt: '2021-03-02T15:16:09.246+00:00',
+  author: {
+    id: '123456',
+    slug: 'gustavo-seabra',
+    displayName: 'Gustavo Seabra',
+  },
+  categories: [
+    {
+      id: '1234',
+      displayName: 'Tech',
+      slug: 'tech',
+    },
+    {
+      id: '5678',
+      displayName: 'JS',
+      slug: 'javascript',
+    },
+  ],
+} as ArticleMetaProps;
